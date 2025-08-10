96e99dc7ae3efd5fab829db10499517d7c1e913b
Paranubhav Dash <dashparanubhav26@gmail.com>
2025-08-11 03:14:10 +0530

    Delete S-Box_Static.py

--- S-Box_Static.py
+++
@@ -1,7 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-
-def encrypt_aes_static(plaintext: bytes, key: bytes) -> bytes:
-    cipher = AES.new(key, AES.MODE_ECB)
-    padded = pad(plaintext, AES.block_size)
-    return cipher.encrypt(padded)
